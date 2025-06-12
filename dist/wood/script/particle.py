8eed140b2fbf2695011f10e7c626cbac01b16cf0
thanh5767 <tranlexuanthanh880@gmail.com>
2025-06-12 20:54:41 +0700

    first commit

--- dist/wood/script/particle.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Particle():
-    def __init__(self,shape,pos,time):
-        self.shape = shape 
-        self.pos = pos
-        self.time = time
-    def render(self,surf):
-        pass
